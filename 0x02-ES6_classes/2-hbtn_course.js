86041389e25dd94126c98e9d6da181d75e27bb45
MutabPato <patrickmtbz@gmail.com>
2024-06-27 20:43:56 +0000

    JS classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,6 +21,9 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('name must be a string');
+    }
     this._name = newName;
   }
 
@@ -29,6 +32,9 @@ export default class HolbertonCourse {
   }
 
   set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('length must be a number');
+    }
     this._length = newLength;
   }
 
@@ -37,6 +43,9 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('students must be an array');
+    }
     this._students = newStudents;
   }
 }
