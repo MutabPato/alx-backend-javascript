5ffc4433c190a3c6f888a3980713a3e151476816
MutabPato <patrickmtbz@gmail.com>
2024-06-29 20:36:15 +0000

    Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+
+  try {
+    const result = numerator / denominator;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
