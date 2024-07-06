479d854e5e35d72a20d4458f171aa6bbad7a520c
MutabPato <patrickmtbz@gmail.com>
2024-07-06 19:19:18 +0000

    Student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -22,5 +22,29 @@ let student2: Student = {
 const studentsList: Student[] = [student1, student2];
 
 // Render table
+document.addEventListner("DOMContentLoaded", () => {
+	const table = document.createElement("table");
+	const tableHeader = table.createTHead();
+	const headerRow = tableHeader.insertRow();
 
+	const headerCell1 = document.createElement("th");
+	headerCell1.textContent = "First Name";
+	headerRow.appendChild(headerCell1);
 
+	const headerCell2 = document.createElement("th");
+	headerCell2.textContent = "Location";
+	headerRow.appendChild(headerCell2);
+
+	const tableBody = table.createTBody();
+	studentsList.forEach((student) => {
+		const row = tableBody.insertRow();
+
+		const cell1 = row.insertCell();
+		cell1.textContent = student.firstName;
+
+		const cell2 = row.insertCell();
+                cell2.textContent = student.location;
+	});
+	
+	document.body.appendChild(table);
+});
