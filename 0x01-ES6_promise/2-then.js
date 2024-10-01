d5ef000335750fd0be7f5af4b1f657136b0669ca
MutabPato <patrickmtbz@gmail.com>
2024-10-01 17:18:58 +0000

    Handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
-
-handleResponseFromAPI(true)
-  .then(() => {
-    console.log('Got a response from the API');
-  })
-  .catch((error) => {
-    console.error(error.message);
-  })
-  .finally(() => {
-  });
