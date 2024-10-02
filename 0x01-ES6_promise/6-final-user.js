7e43fddee40d7b0949d2a8bf1faeb302b2fc0fb3
MutabPato <patrickmtbz@gmail.com>
2024-10-02 14:04:24 +0000

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => ([results.status, results.value]));
+    .then((results) => results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        return result.value;
+      }
+      return result.reason;
+    }));
 }
