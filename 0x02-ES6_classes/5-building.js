dd3726b68ec47f551bd569437062bf7401a8016e
MutabPato <patrickmtbz@gmail.com>
2024-10-04 14:40:55 +0000

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,16 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     if (typeof sqft === 'number') { this._sqft = sqft; } else { throw new TypeError('sqft must be a number'); }
   }
 
   get sqft() {
-  return this._sqft;
+    return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
