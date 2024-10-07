525ed7dc2234f827deb093e4e5b0388dcf0984e0
MutabPato <patrickmtbz@gmail.com>
2024-10-07 21:21:26 +0000

    Set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  const subset = new Set(array);
-
-  const instersection = new Set([...subset].filter((x) => set.has(x)));
-
-  if (instersection.size === subset.size) { return true; } return false;
+  return array.every((element) => set.has(element));
 }
