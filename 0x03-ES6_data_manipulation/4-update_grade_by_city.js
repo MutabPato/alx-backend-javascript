42bacdb8ed3b57d5fc84edbef45e1df7712f5d48
MutabPato <patrickmtbz@gmail.com>
2024-10-07 16:58:11 +0000

    Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  const updatedStudents = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.newGrades === student.id);
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
+  return updatedStudents;
+}
