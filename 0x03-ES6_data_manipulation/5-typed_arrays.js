b8ef605121437451562ef96eb8d89f308e391935
MutabPato <patrickmtbz@gmail.com>
2024-10-07 19:59:12 +0000

    Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  int8View[position] = value;
+
+  return new DataView(buffer);
+}
